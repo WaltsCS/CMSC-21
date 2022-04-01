da97fb74643b92e2e18fb373358e98be664f17b8
Walton Karl <100638896+WaltsCS@users.noreply.github.com>
2022-04-02 05:08:14 +0800

    Add files via upload

---
+++ CMSC21/assignment_lec1_c2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int minuend = 87, subtrahend = 15;
+
+    int difference = minuend - subtrahend;
+
+    printf("The difference of subtracting 15 from 87 is %d.", difference);
+
+}
